ea7bb2fb852fd383804a2765bd55c6769894cfc8
angela-goncalves <angelakgo20@gmail.com>
2021-06-08 19:20:46 -0300

    was add task component and to show static data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import {useState} from 'react'
 import './index.css';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "this is a task",
+      day: "May at 1th at 2pm",
+      reminder: false, 
+    },
+    {
+      id: 2,
+      title: "this is a second task",
+      day: "July at 7th at 2pm",
+      reminder: false, 
+    },
+    {
+      id: 3,
+      title: "this is a third task",
+      day: "September 4th at 2pm",
+      reminder: false, 
+    }
+  ]);
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
   return (
     <div className="App">
       <Header/>
+      {tasks.length > 0 ? 
+        <Tasks 
+          tasks={tasks}
+          onDelete={deleteTask} 
+        />
+        : <p>No tasks to show</p>
+      }
     </div>
   );
 }
